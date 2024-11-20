ce6841de14fd89e37cd202cc92212b418fa81651
arman islam <armanislam527@gmail.com>
2024-11-21 01:25:45 +0600

    first commite is this repo

---
+++ Command_line_argument.java
@@ -0,0 +1,8 @@
+public class Command_line_argument {
+    public static void main(String[] args){
+        if (exp instanceof Object) {
+            Object obj = (Object)exp;
+            
+        }
+    }
+}
