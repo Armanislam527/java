561d8159094b009831956e4f88cad8d760970f02
arman islam <armanislam527@gmail.com>
2025-02-11 12:02:09 +0600

    book

---
+++ roomarea.java
@@ -0,0 +1,37 @@
+
+class roomarea {
+
+    public static void main(String args[]) {
+        room r = new room();
+        r.getdata(10, 20);
+        float area = r.breadth * r.length;
+        System.out.println("Area of the room is:" + area);
+
+    }
+}
+
+class room {
+
+    float length;
+    float breadth;
+
+    /**
+     * A method to store the length and breadth of the room
+     *
+     * @param l the length of the room
+     * @param b the breadth of the room
+     *
+     **
+     *
+     *
+     * *********** ✨ Codeium Command 🌟 ************
+     */
+    void getdata(float l, float b) {
+        length = l;
+        breadth = b;
+
+        /**
+         * **** de169968-0967-4475-8f94-41f2543f7795 ******
+         */
+    }
+}
--- test1.java
+++ test1.java
@@ -1,4 +1,5 @@
 
+import com.sun.tools.javac.Main;
 import java.lang.Math;
 
 class test1 {
@@ -6,19 +7,9 @@ class test1 {
     public static void main(String[] args) {
         double x = 5;
         double y;
-        y = math.sqrt(x);
-        System.out.println("The square root of " + x + 
+        y = Math.sqrt(x);
+        System.out.println("The square root of " + x
+                + " is " + y);
 
-
-
-    
-
-" is " +  y
-
-
-    
-
-);
-         
-         
+    }
 }
