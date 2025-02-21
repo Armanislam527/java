d88926da5cca863a5c181f4990a589bf0e2def0c
arman islam <armanislam527@gmail.com>
2025-02-21 18:00:44 +0600

    add dataTypesCharacter and keywordThis classes for character representation and keyword demonstration

---
+++ udemyjavacourse/packages/datatypes/dataTypesCharacter.java
@@ -0,0 +1,13 @@
+//package datatypes;
+
+public class dataTypesCharacter {
+
+    public static void main(String[] args) {
+        char c1 = 'A';//directly assigning a character
+        c2 = 65;//ASCII encoding for character 'A'
+        char c3 = '\u0041';//unicode encoding for character 'A'
+        System.out.println("c1: " + c1);
+        System.out.println("c2: " + c2);
+        System.out.println("c3: " + c3);
+    }
+}
---
+++ udemyjavacourse/packages/keywords/keywordThis.java
@@ -0,0 +1,25 @@
+//package keywords;
+
+class Myclass {
+
+    private int a, b;
+
+    public Myclass(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public Myclass() {
+        this(0, 0);
+    }
+
+    public void setdata(int a, int b) {
+    }
+
+    class keywordThis {
+
+        public static void main(String[] args) {
+            A obj = new A();
+            obj.print();
+        }
+    }
